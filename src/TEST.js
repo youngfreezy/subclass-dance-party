32fc66f1078c68ee69d492a40d8d30aca764070a
youngfreezy <Fareez.Ahmed@gmail.com>
2015-08-26 20:21:02 -0700

    Initial commit - Refactored Dancer and added two new subclass dancers

--- src/TEST.js
+++ src/TEST.js
@@ -1,14 +1,15 @@
-// Creates and returns a new dancer object that can step
-var Dancer = function(top, left, timeBetweenSteps){
+var makeDancer = function(top, left, timeBetweenSteps){
 
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this._timeBetweenSteps = timeBetweenSteps;
-  this._top = top;
-  this._left = left;
   this.setPosition(top, left);
   this.step();
+
+
+  
+
+  
 
 
   
@@ -18,26 +19,25 @@ var Dancer = function(top, left, timeBetweenSteps){
 
 
 // add prototype methods
-Dancer.prototype.step = function(){
- 
+makeDancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  // console.log('hello');
-  setTimeout(this.step.bind(this), this._timeBetweenSteps);
+  setTimeout(this.step, timeBetweenSteps);
 };
 
 
 
-Dancer.prototype.setPosition = function(top, left){
+makeDancer.prototype.setPosition = function(top, left){
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
+  //
   var styleSettings = {
     top: top,
     left: left
   };
-  debugger
   this.$node.css(styleSettings);
 };
 
 
 
+var dancer = new makeDancer(10, 10, 100);
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,59 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  Dancer.call(this, top, left, timeBetweenSteps);
+  // copy over properties from Dancer()
+  
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function(){
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
+  
 };
+
+// Setting up inheritance
+makeBlinkyDancer.prototype = Object.create(Dancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+//Overwriting a prototype method
+makeBlinkyDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
+
+var SmoothDancer = function (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+}
+
+SmoothDancer.prototype = Object.create(Dancer.prototype);
+SmoothDancer.prototype.constructor = SmoothDancer;
+
+SmoothDancer.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  var realVertMove = Math.floor((Math.random() * 100) + -50) + 'px';
+  var realHorizMove = Math.floor((Math.random() * 100) + -50) + 'px';
+  Dancer.prototype.step.call(this);
+
+  this.$node.animate({top: realVertMove, left: realHorizMove},5000);
+}
+
+var MoonWalker = function  (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  // body...
+
+}
+
+MoonWalker.prototype = Object.create(Dancer.prototype);
+MoonWalker.prototype.constructor = MoonWalker;
+
+MoonWalker.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  this.$node.animate({left: '0px'}, 9000);
+}
+
+// MoonWalker.prototype.setPosition = function(top, left){
+//   var styleSettings = {left: '0px'};
+//   this.$node.css(styleSettings)
+// }
